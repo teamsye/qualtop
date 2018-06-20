0eca39ad503c4ef19848fedfee789c95e3ee5331
Humberto Arocha <hbto@vauxoo.com>
2018-06-19 23:00:56 -0400

    [IMP] Increasing version number to allow auto update the module

--- qualtop/__manifest__.py
+++ qualtop/__manifest__.py
@@ -1,7 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Qualtop App',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.0.1',
     "author": "Vauxoo",
     "license": "LGPL-3",
     'category': 'Hidden',
